38c3ace3248228c069a8aec9efbb3d8de17575e0
gauranglad <studyuklad@gmail.com>
2025-12-16 06:12:22 +0000

    updated file

--- src/test/java/com/gorest/testsuite/UserAssertionTest.java
+++ src/test/java/com/gorest/testsuite/UserAssertionTest.java
@@ -5,8 +5,7 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.*;
 
 public class UserAssertionTest {
 
@@ -20,20 +19,44 @@ public class UserAssertionTest {
                 .then()
                 .statusCode(200);
     }
-//Verify the if the total record is 20
+
+    //Verify the if the total record is 20
     @Test
-    public void test001(){
-        response.body("$.size()",equalTo(20));
+    public void test001() {
+        response.body("$.size()", equalTo(20));
     }
-//Verify the if the name of id = 5914197 is equal to ”Bhilangana Dhawan”
+
+    //Verify the if the name of id = 8228718 is equal to ”Amb. Gurdev Varrier”
+    @Test
+    public void test002() {
+        response.body("find{it.id==8228718}.name", equalTo("Amb. Gurdev Varrier"));
+    }
+
+    //Check the single ‘Name’ in the Array list (Chandan Verma)
+    @Test
+    public void test003() {
+        response.body("name", hasItem("Chandan Verma"));
+    }
+//Check the multiple ‘Names’ in the ArrayList ("Chandan Verma","Gobinda Gill","Mrs. Sanya Gupta")
 @Test
-public void test002(){
-    response.body("$.find{it.id==5914197}.name",hasItems("Bhilangana Dhawan"));
+public void test004() {
+    response.body("name", hasItems("Chandan Verma","Gobinda Gill","Mrs. Sanya Gupta"));
 }
-//Check the single ‘Name’ in the Array list (Dev Bhattacharya)
-//Check the multiple ‘Names’ in the ArrayList (Usha Kaul Esq., Akshita Mishra, Chetanaanand Reddy )
-//Verify the emai of userid = 5914185 is equal “tandon_iv_aanandinii@prosacco.example”
-//Verify the status is “Active” of user name is “Amaresh Rana”
-//Verify the Gender = male of user name is “Dhanalakshmi Pothuvaal”
+//Verify the email of userid = 8228716 is equal “gill_gobinda@orn-collier.example”
+@Test
+public void test005() {
+    response.body("find{it.id==8228716}.email", equalTo("gill_gobinda@orn-collier.example"));
+}
+//Verify the status is “Active” of user name is “Gobinda Gill”
+@Test
+public void test006() {
+    response.body("find{it.id==8228716}.status", equalTo("active"));
+}
+
+//Verify the Gender = male of user name is “Gobinda Gill”
+@Test
+public void test007() {
+    response.body("find{it.name=='Gobinda Gill'}.gender", equalTo("male"));
+}
 
 }
